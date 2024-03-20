66aa881b34fcfcb7fb7d526f44d92ca46b669a1b
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-20 16:58:05 +0530

    Create p.java

---
+++ p.java
@@ -0,0 +1,18 @@
+package p1;
+class x
+  {
+    int p=2;
+    private int q=3;
+    protected int r=4;
+    public int n=1;
+
+    x
+    {
+      System.out.println("This is constructor");
+      System.out.println(n);
+      System.out.println(p);
+      System.out.println(q);
+      System.out.println(r);
+      
+    }
+  }
