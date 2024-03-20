7ef4e5dc4bf71d3b861aba9d6f5ab3260024a23a
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-20 17:23:05 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,25 @@
+class cal
+  {
+double i;
+  double x;
+    void p()
+    {
+x = Math.sqrt(i);
+      
+      
+    }
+    
+  }
+
+class p2
+  {
+public static void main(String[], args)
+    {
+      cal c = new cal();
+      c.i =3;
+      c.p()
+        System.out.println(c.x);
+      
+    }
+    
+  }
